f4518e8df59cacc9c7917dc75e17f5718b725aeb
jml <jml@gmail.com>
2024-09-19 09:36:17 -0300

    .

--- functios/list_functios.js
+++ functios/list_functios.js
@@ -3,7 +3,7 @@ const listar = function() {
     console.log("La funcion 'list' lista los elementos de 'Data.json' ");
     console.log("La función 'find' busca un elemento de 'Data.json' ");
     console.log("La función 'filter' busca un elemento de 'Data.json y lo de devuelve como un objeto' ");
-    console.log("seguir describiendo funciones' " 
+    console.log("seguir describiendo funciones' " );
     }   
 
 module.exports = listar;
